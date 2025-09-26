452d75466b1eb40a42d67c01400190d3f4a883cb
Muhammad Zain <muhammadzainrajput82@gmail.com>
2025-09-26 16:07:39 +0500

    code updated

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,19 +3,20 @@ import Image from "next/image"
 
 type ButtonProps = {
   type: 'button' | 'submit'
-  title: string
-  icon?: string
-  variant: string
+  title: string;
+  icon?: string;
+  variant: string;
+  full?: boolean;
 }
 
-const Button = ({ type, title, icon, variant}: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full}: ButtonProps) => {
   return (
     <button 
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
       type={type}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-      <span className="bold-16 whitespace-nowrap">{title}</span>
+      <span className="bold-16 whitespace-nowrap cursor-pointer">{title}</span>
     </button>
   )
 }
